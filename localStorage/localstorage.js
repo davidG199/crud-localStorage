4a1d166d003ffb97ea5002801d3138baf46eb006
David gomez <david.gomez04@hotmail.com>
2024-04-12 16:24:38 -0500

    se agrego boton de eliminar y actualizar datos

--- localStorage/localstorage.js
+++ localStorage/localstorage.js
@@ -9,9 +9,11 @@ let tabla = document.querySelector(".table tbody");
 //evento para el boton guardar
 botonGuardar.addEventListener("click", function () {
   let datos = obtenerDatos();
-  guardarDatos(datos);
-  borrarTabla()
-  mostrarDatos()
+  if (datos != null) {
+    guardarDatos(datos);
+  }
+  borrarTabla();
+  mostrarDatos();
 });
 
 //funcion para tomar los datos del formulario
@@ -19,27 +21,24 @@ function obtenerDatos() {
   let producto = {};
   //validar que el formulario este lleno
   if (
-    nombrePro.value.trim() !== "" &&
-    precioPro.value.trim() !== "" &&
-    presentacionPro.value.trim() !== "" &&
-    imagenPro.value.trim() !== ""
+    nombrePro.value == "" ||
+    precioPro.value == "" ||
+    presentacionPro == "" ||
+    imagenPro.value == ""
   ) {
+    alert("Todos los datos son obligatorios");
+    return;
+  } else {
     producto = {
       nombre: nombrePro.value,
       precio: precioPro.value,
       presentacion: presentacionPro.value,
       imagen: imagenPro.value,
     };
-  } else {
-    alert("Todos los datos son obligatorios");
+    console.log(producto);
+    limpiarForm();
+    return producto;
   }
-
-  console.log(producto);
-  nombrePro.value = "";
-  precioPro.value = "";
-  presentacionPro.value = "";
-  imagenPro.value = "";
-  return producto;
 }
 
 //funcion para guardar los datos en localStorage
@@ -76,32 +75,87 @@ function mostrarDatos() {
             <td>${p.precio}</td>
             <td>${p.presentacion}</td>
             <td> <img src="${p.imagen}" width="50%"> </td>
-            <td> <span class="btn"> 🖊️</span> </td>
-            <td> <span class="btn"> ❌</span> </td>
+            <td> <span onclick="actualizarPedido(${i})" class="btn"> 🖊️</span> </td>
+            <td> <span onclick="eliminarPedido(${i})" class="btn"> ❌</span> </td>
         `;
     tabla.appendChild(fila);
   });
 }
 
-function borrarTabla(){
-  let filas = document.querySelectorAll(".table tbody tr")
+function borrarTabla() {
+  let filas = document.querySelectorAll(".table tbody tr");
 
   filas.forEach((f) => {
-    f.remove()
-  })
+    f.remove();
+  });
 }
 
+//eliminar pedido de la tabla
+function eliminarPedido(pos) {
+  let pedidos = [];
+
+  let pedidosPrevios = JSON.parse(localStorage.getItem("Pedidos"));
+
+  if (pedidosPrevios != null) {
+    pedidos = pedidosPrevios;
+  }
+
+  let confirmar = confirm(`¿Deseas eliminar el pedido ${pedidos[pos].nombre}?`);
+  if (confirmar) {
+    pedidos.splice(pos, 1);
+    alert("Pedido eliminado con exito");
+    //guardar los datos que quedan en el localStorage
+    localStorage.setItem("Pedidos", JSON.stringify(pedidos));
+    borrarTabla();
+    mostrarDatos();
+  }
+}
+
+//actualizar pedido
+function actualizarPedido(pos) {
+  let pedidos = [];
+
+  let pedidosPrevios = JSON.parse(localStorage.getItem("Pedidos"));
+
+  if (pedidosPrevios != null) {
+    pedidos = pedidosPrevios;
+  }
+
+  nombrePro.value = pedidos[pos].nombre;
+  precioPro.value = pedidos[pos].precio;
+  imagenPro.value = pedidos[pos].imagen;
+  presentacionPro.value = pedidos[pos].presentacion;
+
+  let btnActualizar = document.querySelector(".btn-actualizar");
+  btnActualizar.classList.toggle("d-none");
+  botonGuardar.classList.toggle("d-none");
+
+  btnActualizar.addEventListener("click", function () {
+    pedidos[pos].nombre = nombrePro.value;
+    pedidos[pos].precio = precioPro.value;
+    pedidos[pos].presentacion = presentacionPro.value;
+    pedidos[pos].imagen = imagenPro.value;
+    localStorage.setItem("Pedidos", JSON.stringify(pedidos));
+    alert("Dato actualizado con exito");
+    limpiarForm();
+
+    btnActualizar.classList.toggle("d-none")
+    botonGuardar.classList.toggle("d-none")
+    borrarTabla();
+    mostrarDatos();
+  });
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-  borrarTabla()
+  borrarTabla();
   mostrarDatos();
 });
 
 // funcion para limpiar el formulario
 
-// function limpiarForm (){
-//     nombrePro.value = "";
-//     precioPro.value = "";
-//     presentacionPro.value = "";
-//     imagenPro.value = "";
-// }
+function limpiarForm() {
+  nombrePro.value = "";
+  precioPro.value = "";
+  presentacionPro.value = "";
+  imagenPro.value = "";
+}
